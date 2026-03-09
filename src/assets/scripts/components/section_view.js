81ef27c2446cfb4d58f722ecff5d44cae18e0756
Sofi Dobriak <sofi.dobriak@gmail.com>
2026-03-09 16:11:35 +0200

    add a check for the presence of a button in the section_view.js file

--- src/assets/scripts/components/section_view.js
+++ src/assets/scripts/components/section_view.js
@@ -1,10 +1,13 @@
 if (document.querySelector('.index_page')) {
-  document.getElementById('loadIframeButton').addEventListener('click', function() {
-    var iframe = document.getElementById('lazyIframe');
-    iframe.src = iframe.getAttribute('data-src');
-    iframe.style.display = 'block';
+  const target = document.getElementById('loadIframeButton');
+  if (target) {
+    target.addEventListener('click', function() {
+      var iframe = document.getElementById('lazyIframe');
+      iframe.src = iframe.getAttribute('data-src');
+      iframe.style.display = 'block';
 
-    this.style.display = 'none';
-    document.querySelector('.iframe-container').classList.add('active-iframe');
-  });
+      this.style.display = 'none';
+      document.querySelector('.iframe-container').classList.add('active-iframe');
+    });
+  }
 }
